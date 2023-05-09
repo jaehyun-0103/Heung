4ec89a9fae7634dcc82d6f98722eac82a348de29
조재현 <80446951+jaehyun-0103@users.noreply.github.com>
2023-05-09 13:02:48 +0900

    Main

---
+++ Heung/app/src/main/java/com/example/heung/RecruitActivity.kt
@@ -0,0 +1,11 @@
+package com.example.heung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecruitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recruit)
+    }
+}
---
+++ Heung/app/src/main/java/com/example/heung/RentActivity.kt
@@ -0,0 +1,11 @@
+package com.example.heung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rent)
+    }
+}
