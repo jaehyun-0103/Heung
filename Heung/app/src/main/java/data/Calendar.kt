cfd9a94b738645811803edf1859bdfc98bf484dc
조재현 <80446951+jaehyun-0103@users.noreply.github.com>
2023-05-26 22:08:00 +0900

    jaehyun8

---
+++ Heung/app/src/main/java/data/Calendar.kt
@@ -0,0 +1,12 @@
+package data
+
+data class Calendar(
+    val cal_id: String = "",
+    val user_id: String = "",
+    val cal_title: String = "",
+    val cal_memo: String = "",
+    val cal_date: String = "",
+    val cal_startDate: String = "",
+    val cal_endDate: String = "",
+    val cal_location: String = "",
+)
