9d790152019cae8c2e8518c66e268205a86e257d
tonykorea99 <tonykorea99@naver.com>
2023-05-30 05:56:17 +0900

    seok-1

--- Heung/app/src/main/java/com/example/heung/MainActivity.kt
+++ Heung/app/src/main/java/com/example/heung/MainActivity.kt
@@ -1,19 +1,61 @@
-package com.example.heung
-
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.content.Intent
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.heung.databinding.ActivityMainBinding
+import com.example.heung.R
+import com.example.heung.CalActivity
+import com.example.heung.RecruListActivity
+import com.example.heung.RentActivity
+import com.example.heung.SelfProfActivity
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val btn_postlist = findViewById<Button>(R.id.btn_postlist)
-        btn_postlist.setOnClickListener {
-            val intent = Intent(this, PostListActivity::class.java)
-            startActivity(intent)
+        navView = binding.bottomNavigation
+
+        navView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_main -> {
+                    // Main 탭 클릭 시 MainActivity로 이동
+                    val intent = Intent(this, MainActivity::class.java)
+                    if (item.isChecked) {
+                        startActivity(intent)
+                        finish()
+                    }
+                }
+                R.id.nav_calendar -> {
+                    // Calendar 탭 클릭 시 CalActivity로 이동
+                    val intent = Intent(this, CalActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.nav_map -> {
+                    // Map 탭 클릭 시 RentActivity로 이동
+                    val intent = Intent(this, RentActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.nav_profile -> {
+                    // Profile 탭 클릭 시 SelfProfActivity로 이동
+                    val intent = Intent(this, SelfProfActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.nav_recruit -> {
+                    // Recruit 탭 클릭 시 RecruitListActivity로 이동
+                    val intent = Intent(this, RecruListActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+            true
         }
     }
 }
