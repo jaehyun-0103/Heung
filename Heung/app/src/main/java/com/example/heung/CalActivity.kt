c0d154b2a409be71940b46f99cbdf27cc14e7fb1
tonykorea99 <tonykorea99@naver.com>
2023-05-30 06:00:05 +0900

    seok-2

--- Heung/app/src/main/java/com/example/heung/CalActivity.kt
+++ Heung/app/src/main/java/com/example/heung/CalActivity.kt
@@ -2,6 +2,10 @@ package com.example.heung
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.heung.databinding.ActivityMainBinding
+import com.example.heung.R
 
 class CalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
