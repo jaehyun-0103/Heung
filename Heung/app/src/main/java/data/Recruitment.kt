0ad71bb3a3845cddf3f43643da46c8b113420fc8
조재현 <80446951+jaehyun-0103@users.noreply.github.com>
2023-05-30 11:35:48 +0900

    Delete Recruitment.kt

--- Heung/app/src/main/java/data/Recruitment.kt
+++
@@ -1,8 +0,0 @@
-package data
-
-data class Recruitment(
-    val recruitment_id: String = "",
-    val recruit_id: String = "",
-    val user_id: String = "",
-    val recruit_status: String = ""
-)
