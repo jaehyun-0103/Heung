00912e99b10bb130e70d922c3cd32fe1e88ed90c
44tak <132973042+44tak@users.noreply.github.com>
2023-06-03 23:15:42 +0900

    2 // Recruit List, Write Activity
    변경점
    1. List 전체글 보기 버튼 추가. 기능확인
    2. List recurit_id 가 높은 순서대로 게시글에 보임 (최신글이 가장위에 보임)
    3. List recruit_id가 없으면 보이지 않도록 예외처리
    4. Write 에서 작성한 내용 모두 db에 올라감 확인
    5. 비어있는 내용 있으면 작성 안되고 토스트 뜨도록 예외처리
    6. 모집기간 누르면 캘린더 뜨고, 고른 날짜 recruit_endDate에 저장
    7. 모집인원 누르면 숫자고르는거 뜨고, 고른 숫자 recruit_max 에 저장.
    8. 게시글 작성하면 recruit_curr = 0 되도록 설정. (클래스 선생은 빼고 생각. 과외 4명이라고 하면 선생 빼고 생각하듯이)
    9. recruit_session, recruit_class 둘 중 하나는 필터 선택에 의해 무조건 비는 내용

--- Heung/app/src/main/java/com/example/heung/RecrutWriteActivity.kt
+++ Heung/app/src/main/java/com/example/heung/RecrutWriteActivity.kt
@@ -1,14 +1,20 @@
 package com.example.heung
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatImageButton
-import androidx.appcompat.widget.AppCompatButton
 import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.NumberPicker
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatImageButton
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import data.Recruits
+import java.text.SimpleDateFormat
+import java.util.*
 
 class RecrutWriteActivity : AppCompatActivity() {
 
@@ -20,29 +26,36 @@ class RecrutWriteActivity : AppCompatActivity() {
     private lateinit var classFilterButton: AppCompatButton
     private lateinit var buskingLayout: LinearLayout
     private lateinit var classLayout: LinearLayout
-    private var selectedFilter: String = "" // 선택된 필터를 저장하는 변수
+    private lateinit var buskingDateEditText: EditText
+    private lateinit var buskingSessionEditText: EditText
+    private lateinit var classTypeEditText: EditText
+    private lateinit var classDate2EditText: EditText
+    private lateinit var maxCapacityPicker: NumberPicker
     private lateinit var firestore: FirebaseFirestore
+    private var selectedFilter: String = ""
+    private var selectedDate: String = ""
+    private var nextRecruitId: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recrutwrite)
 
-        // Firebase Firestore 초기화
         firestore = FirebaseFirestore.getInstance()
 
-        // 뷰 초기화
         titleEditText = findViewById(R.id.recruit_title)
         contentEditText = findViewById(R.id.recruit_content)
         uploadButton = findViewById(R.id.recruit_upload)
         backButton = findViewById(R.id.recruit_btn_back)
-
         buskingFilterButton = findViewById(R.id.recruit_filter_busking)
         classFilterButton = findViewById(R.id.recruit_filter_class)
-
         buskingLayout = findViewById(R.id.recruit_filter_busking_layout)
         classLayout = findViewById(R.id.recruit_filter_class_layout)
+        buskingDateEditText = findViewById(R.id.recruit_filter_busking_date)
+        buskingSessionEditText = findViewById(R.id.recruit_filter_busking_session)
+        classTypeEditText = findViewById(R.id.recruit_filter_class_type)
+        classDate2EditText = findViewById(R.id.recruit_filter_class_date2)
+        maxCapacityPicker = findViewById(R.id.recruit_filter_busking_capacity)
 
-        // 필터 버튼 클릭 리스너 설정
         buskingFilterButton.setOnClickListener {
             selectFilter("버스킹")
         }
@@ -51,66 +64,159 @@ class RecrutWriteActivity : AppCompatActivity() {
             selectFilter("클래스")
         }
 
-        // 업로드 버튼 클릭 리스너 설정
         uploadButton.setOnClickListener {
-            // 모집글 업로드 처리
             uploadRecruit()
         }
 
-        // 뒤로 가기 버튼 클릭 리스너 설정
         backButton.setOnClickListener {
             onBackPressed()
         }
+
+        buskingDateEditText.setOnClickListener { view ->
+            showDatePickerDialog(view)
+        }
+
+        // NumberPicker 설정
+        maxCapacityPicker.minValue = 0
+        maxCapacityPicker.maxValue = 100
+
+        getNextRecruitId()
     }
 
-    // 필터 선택
     private fun selectFilter(filter: String) {
         selectedFilter = filter
-        // 선택된 필터에 따라 UI 업데이트
         if (filter == "버스킹") {
             buskingLayout.visibility = View.VISIBLE
             classLayout.visibility = View.GONE
+            buskingDateEditText.setText(selectedDate)
         } else if (filter == "클래스") {
             buskingLayout.visibility = View.GONE
             classLayout.visibility = View.VISIBLE
+            classDate2EditText.setText(selectedDate)
         }
     }
 
-    private fun uploadRecruit() {
-        // 제목과 내용을 에딧텍스트에서 가져옴
-        val title = titleEditText.text.toString()
-        val content = contentEditText.text.toString()
+    fun showDatePickerDialog(view: View) {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
 
-        // 필터 선택 여부 확인
+        val datePickerDialog = DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                val selectedMonth = monthOfYear + 1
+                selectedDate = "$year-$selectedMonth-$dayOfMonth"
+
+                if (selectedFilter == "버스킹" || selectedFilter == "클래스") {
+                    buskingDateEditText.setText(selectedDate)
+                    classDate2EditText.setText(selectedDate)
+                }
+            }, year, month, day)
+
+        val currentDate = Calendar.getInstance()
+        val initialYear = currentDate.get(Calendar.YEAR)
+        val initialMonth = currentDate.get(Calendar.MONTH)
+        val initialDay = currentDate.get(Calendar.DAY_OF_MONTH)
+        datePickerDialog.updateDate(initialYear, initialMonth, initialDay)
+
+        datePickerDialog.show()
+    }
+
+    private fun getNextRecruitId() {
+        firestore.collection("Recruits")
+            .orderBy("recruit_id", Query.Direction.DESCENDING)
+            .limit(1)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                if (!querySnapshot.isEmpty) {
+                    val lastRecruit = querySnapshot.documents[0].toObject(Recruits::class.java)
+                    nextRecruitId = if (lastRecruit != null) {
+                        (lastRecruit.recruit_id.toIntOrNull()?.plus(1) ?: 1).toString()
+                    } else {
+                        "1"
+                    }
+                }
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(this, "게시글 번호 가져오기 실패: $e", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun uploadRecruit() {
+        val title = titleEditText.text.toString().trim()
+        val content = contentEditText.text.toString().trim()
+
+        if (title.isEmpty() || content.isEmpty()) {
+            Toast.makeText(this, "제목과 내용을 입력해주세요.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
         if (selectedFilter.isEmpty()) {
             Toast.makeText(this, "필터를 선택해주세요.", Toast.LENGTH_SHORT).show()
             return
         }
 
-        // 모집글 데이터 생성
-        val recruitData = hashMapOf(
-            "recruit_title" to title,
-            "recruit_content" to content,
-            "recruit_type" to selectedFilter // 선택된 필터 값 추가
+        val endDate = if (selectedFilter == "버스킹") {
+            buskingDateEditText.text.toString()
+        } else {
+            classDate2EditText.text.toString()
+        }
+
+        if (endDate.isEmpty()) {
+            Toast.makeText(this, "모집 기간을 선택해주세요.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val currentDate = Calendar.getInstance().apply {
+            timeZone = TimeZone.getTimeZone("Your_Time_Zone")
+        }.time
+
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        val formattedDate = dateFormat.format(currentDate)
+
+        val recruitSession = buskingSessionEditText.text.toString()
+        val recruitClass = classTypeEditText.text.toString()
+
+        if (selectedFilter == "버스킹" && recruitSession.isEmpty()) {
+            Toast.makeText(this, "세션 타입을 입력해주세요.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        if (selectedFilter == "클래스" && maxCapacityPicker.value == 0) {
+            Toast.makeText(this, "수강인원을 입력해주세요.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val maxCapacity = maxCapacityPicker.value.toString()
+
+        val recruit = Recruits(
+            recruit_id = nextRecruitId,
+            recruit_title = title,
+            recruit_content = content,
+            recruit_type = selectedFilter,
+            recruit_endDate = endDate,
+            recruit_date = formattedDate,
+            recruit_session = recruitSession,
+            recruit_class = recruitClass,
+            recruit_curr = "0",
+            recruit_max = maxCapacity
         )
 
-        // Firebase Firestore에 모집글 업로드
         firestore.collection("Recruits")
-            .add(recruitData)
+            .add(recruit)
             .addOnSuccessListener { documentReference ->
-                // 업로드 성공
                 Toast.makeText(this, "모집글이 업로드되었습니다!", Toast.LENGTH_SHORT).show()
-                // 이전 activity로 돌아가기
                 finish()
             }
             .addOnFailureListener { e ->
-                // 업로드 실패
                 Toast.makeText(this, "모집글 업로드 실패: $e", Toast.LENGTH_SHORT).show()
             }
     }
 
+
+
+
     override fun onBackPressed() {
-        // 뒤로 가기 버튼 클릭 시 이전 activity로 돌아가기
         finish()
     }
 }
